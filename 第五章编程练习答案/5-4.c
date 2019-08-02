9419ad72ba97be04adc4dd43c99dacaf5fc9ba31
zzwchn <zzwchn@163.com>
2019-08-02 22:01:13 +0800

    第五章编程练习答案-4题-完善

--- 第五章编程练习答案/5-4.c
+++ 第五章编程练习答案/5-4.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 int main()
 {
+	int height;
 	
+	printf("请输入身高cm：");
+	scanf("%d",&height); 
+	
+	while(height>0){
+		printf("身高为%dcm,合%.3lf英尺\n",height,height*0.0328084);
+		printf("Enter a height in centimeters (<=0 to quit):");
+		scanf("%d",&height); 
+	} 
+	
+	return 0;
 }
